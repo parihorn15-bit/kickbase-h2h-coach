4b406e7646d64b31f1b4e978b04faf3b9d150af2
parihorn15-bit <pari.horn15@gmail.com>
2026-08-04 02:46:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='h2h-coach-cloud-v1';
+const CACHE='h2h-coach-cloud-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./cloud.js','./config.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
