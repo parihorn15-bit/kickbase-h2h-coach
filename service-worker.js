9695c1e68b37f2434316342857319d9876e83790
parihorn15-bit <pari.horn15@gmail.com>
2026-08-14 00:07:41 +0200

    Version 2.0.5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'h2h-coach-cloud-v204';
+const CACHE_NAME = 'h2h-coach-cloud-v205';
 const APP_SHELL = [
   './',
   './index.html',
@@ -20,7 +20,7 @@ self.addEventListener('activate', event => {
       .then(() => self.clients.claim())
       .then(async () => {
         const clients = await self.clients.matchAll({type:'window', includeUncontrolled:true});
-        for (const client of clients) client.postMessage({type:'APP_UPDATED', version:'2.0.4'});
+        for (const client of clients) client.postMessage({type:'APP_UPDATED', version:'2.0.5'});
       })
   );
 });
