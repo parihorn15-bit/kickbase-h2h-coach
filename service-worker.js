b9fc309b4a263a748815fdc68d2b570834f71230
parihorn15-bit <pari.horn15@gmail.com>
2026-08-18 12:25:30 +0200

    Cache Phase 2.3.0 Dev7 runtime

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,19 @@
-const CACHE_NAME='h2h-coach-cloud-v230dev5';
+const CACHE_NAME='h2h-coach-cloud-v230dev7';
 const CORE=[
   './',
   './index.html',
   './styles.css?v=215n',
   './config.js?v=215n',
   './app.js?v=215n',
-  './cloud.js?v=230dev5',
-  './phase230.js?v=230dev5',
+  './cloud.js?v=230dev6',
+  './phase230.js?v=230dev7',
   './phase230-dev1.js?v=230dev1',
   './phase230-dev2.js?v=230dev2',
   './phase230-dev3.js?v=230dev3',
   './phase230-dev4.js?v=230dev4',
   './phase230-dev5.js?v=230dev5',
+  './phase230-dev6.js?v=230dev6',
+  './phase230-dev7.js?v=230dev7',
   './manifest.webmanifest'
 ];
 
@@ -27,7 +29,7 @@ self.addEventListener('activate',event=>{
       .then(()=>self.clients.claim())
       .then(async()=>{
         const clients=await self.clients.matchAll({type:'window',includeUncontrolled:true});
-        for(const client of clients) client.postMessage({type:'APP_UPDATED',version:'2.3.0-test-dev5'});
+        for(const client of clients) client.postMessage({type:'APP_UPDATED',version:'2.3.0-test-dev7'});
       })
   );
 });
