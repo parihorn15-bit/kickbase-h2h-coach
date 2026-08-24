4137759eda847c2cc46984644b8e4ea690a6ddd9
parihorn15-bit <pari.horn15@gmail.com>
2026-08-24 20:10:35 +0200

    chore: load observer-free dev10/dev11 runtime

--- phase230.js
+++ phase230.js
@@ -18,7 +18,7 @@
     .then(()=>load('phase230-dev7.js?v=230dev7','phase230dev7'))
     .then(()=>load('phase230-dev8.js?v=230dev8','phase230dev8'))
     .then(()=>load('phase230-dev9.js?v=230dev9','phase230dev9'))
-    .then(()=>load('phase230-dev10.js?v=230dev10_2','phase230dev10'))
-    .then(()=>load('phase230-dev11.js?v=230dev11_1','phase230dev11'))
+    .then(()=>load('phase230-dev10.js?v=230dev10_3','phase230dev10'))
+    .then(()=>load('phase230-dev11.js?v=230dev11_2','phase230dev11'))
     .catch(error=>console.error('2.3 runtime load failed',error));
 })();
