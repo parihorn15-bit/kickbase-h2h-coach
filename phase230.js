d2fd57a7f3e905dc167d919920c03e59528fc727
parihorn15-bit <pari.horn15@gmail.com>
2026-08-24 19:52:42 +0200

    Load phase 2.3 dev11 UI polish

--- phase230.js
+++ phase230.js
@@ -19,5 +19,6 @@
     .then(()=>load('phase230-dev8.js?v=230dev8','phase230dev8'))
     .then(()=>load('phase230-dev9.js?v=230dev9','phase230dev9'))
     .then(()=>load('phase230-dev10.js?v=230dev10_2','phase230dev10'))
+    .then(()=>load('phase230-dev11.js?v=230dev11_1','phase230dev11'))
     .catch(error=>console.error('2.3 runtime load failed',error));
 })();
