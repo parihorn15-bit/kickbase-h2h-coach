41c2555ff95862bb9ff90e54f753378d553a84f2
parihorn15-bit <pari.horn15@gmail.com>
2026-08-27 12:12:51 +0200

    fix: keep polish cache key aligned

--- phase230.js
+++ phase230.js
@@ -1,8 +1,7 @@
 (() => {
   const load=(src,tag)=>new Promise((resolve,reject)=>{
     if(document.querySelector(`script[data-${tag}]`)){resolve();return;}
-    const s=document.createElement('script');
-    s.src=src;s.async=false;s.dataset[tag]='1';s.onload=resolve;s.onerror=reject;document.head.appendChild(s);
+    const s=document.createElement('script');s.src=src;s.async=false;s.dataset[tag]='1';s.onload=resolve;s.onerror=reject;document.head.appendChild(s);
   });
   load('phase230-dev1.js?v=230dev1','phase230dev1')
     .then(()=>load('phase230-dev2.js?v=230dev2','phase230dev2'))
@@ -14,7 +13,7 @@
     .then(()=>load('phase230-dev8.js?v=230dev8_3','phase230dev8'))
     .then(()=>load('phase230-dev9.js?v=230dev9_3','phase230dev9'))
     .then(()=>load('phase230-dev10.js?v=230dev10_4','phase230dev10'))
-    .then(()=>load('phase230-dev11.js?v=230dev11_9','phase230dev11'))
+    .then(()=>load('phase230-dev11.js?v=230dev11_8','phase230dev11'))
     .then(()=>load('phase230-dev12.js?v=230dev12_1','phase230dev12'))
     .then(()=>load('phase230-dev13.js?v=230dev13_0','phase230dev13'))
     .then(()=>load('phase230-dev14.js?v=230dev14_1','phase230dev14'))
