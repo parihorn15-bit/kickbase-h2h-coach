3b92adef532b5d7e028c97216fe810e58d7e17d7
parihorn15-bit <pari.horn15@gmail.com>
2026-08-27 11:47:17 +0200

    chore: load full-use runtime

--- phase230.js
+++ phase230.js
@@ -13,12 +13,12 @@
     .then(()=>load('phase230-dev2.js?v=230dev2','phase230dev2'))
     .then(()=>load('phase230-dev3.js?v=230dev3','phase230dev3'))
     .then(()=>load('phase230-dev4.js?v=230dev4','phase230dev4'))
-    .then(()=>load('phase230-dev5.js?v=230dev5','phase230dev5'))
+    .then(()=>load('phase230-dev5.js?v=230dev5_1','phase230dev5'))
     .then(()=>load('phase230-dev6.js?v=230dev6','phase230dev6'))
     .then(()=>load('phase230-dev7.js?v=230dev7','phase230dev7'))
     .then(()=>load('phase230-dev8.js?v=230dev8_3','phase230dev8'))
     .then(()=>load('phase230-dev9.js?v=230dev9_3','phase230dev9'))
     .then(()=>load('phase230-dev10.js?v=230dev10_4','phase230dev10'))
-    .then(()=>load('phase230-dev11.js?v=230dev11_6','phase230dev11'))
+    .then(()=>load('phase230-dev11.js?v=230dev11_7','phase230dev11'))
     .catch(error=>console.error('2.3 runtime load failed',error));
 })();
