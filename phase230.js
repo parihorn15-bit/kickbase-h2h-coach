9c004a7ee6504c52675d363f94e9714982e8decb
parihorn15-bit <pari.horn15@gmail.com>
2026-08-27 11:43:24 +0200

    chore: load canonical opponent runtime fixes

--- phase230.js
+++ phase230.js
@@ -16,9 +16,9 @@
     .then(()=>load('phase230-dev5.js?v=230dev5','phase230dev5'))
     .then(()=>load('phase230-dev6.js?v=230dev6','phase230dev6'))
     .then(()=>load('phase230-dev7.js?v=230dev7','phase230dev7'))
-    .then(()=>load('phase230-dev8.js?v=230dev8_2','phase230dev8'))
-    .then(()=>load('phase230-dev9.js?v=230dev9_2','phase230dev9'))
-    .then(()=>load('phase230-dev10.js?v=230dev10_3','phase230dev10'))
-    .then(()=>load('phase230-dev11.js?v=230dev11_5','phase230dev11'))
+    .then(()=>load('phase230-dev8.js?v=230dev8_3','phase230dev8'))
+    .then(()=>load('phase230-dev9.js?v=230dev9_3','phase230dev9'))
+    .then(()=>load('phase230-dev10.js?v=230dev10_4','phase230dev10'))
+    .then(()=>load('phase230-dev11.js?v=230dev11_6','phase230dev11'))
     .catch(error=>console.error('2.3 runtime load failed',error));
 })();
