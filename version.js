2d7ef41b27ebc87adb4cff5eb657f8b419068060
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 23:11:23 +0200

    Bump asset key for duplicate cleanup and scoped ownership UI

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300statecore1',
-    cacheName: 'h2h-coach-v300-statecore1'
+    assetKey: '300statecore2',
+    cacheName: 'h2h-coach-v300-statecore2'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
