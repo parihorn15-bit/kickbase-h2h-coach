686e9407ee0a225f782f6c2949eb0224c0dc0665
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 23:04:03 +0200

    Bump centralized state core asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300identityrepair2',
-    cacheName: 'h2h-coach-v300-identityrepair2'
+    assetKey: '300statecore1',
+    cacheName: 'h2h-coach-v300-statecore1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
