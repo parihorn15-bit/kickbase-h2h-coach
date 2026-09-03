a50791a488d3fb3a56f8577d7d7df20fb84bed94
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 22:57:38 +0200

    Bump identity repair asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300statesync1',
-    cacheName: 'h2h-coach-v300-statesync1'
+    assetKey: '300identityrepair2',
+    cacheName: 'h2h-coach-v300-identityrepair2'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
