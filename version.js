6f4f9bf84ab2bd4186cac840d1bd75298f56828b
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 23:21:25 +0200

    Bump asset key for startup and history repair

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300statecore2',
-    cacheName: 'h2h-coach-v300-statecore2'
+    assetKey: '300statecore3',
+    cacheName: 'h2h-coach-v300-statecore3'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
