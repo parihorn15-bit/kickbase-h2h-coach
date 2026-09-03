10d74f191137572b90f58bec5b897e5e708b23be
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 22:22:46 +0200

    Bump Horn transfer history asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300ownershipfix1',
-    cacheName: 'h2h-coach-v300-ownershipfix1'
+    assetKey: '300hornhistory1',
+    cacheName: 'h2h-coach-v300-hornhistory1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
