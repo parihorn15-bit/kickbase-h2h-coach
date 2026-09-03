394687940e2369c1d534f1a5c4756105d149eeaf
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 18:35:14 +0200

    Bump 3.0 asset key for Fäps transfer history

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300anchors2',
-    cacheName: 'h2h-coach-v300-anchors2'
+    assetKey: '300fapstransfers1',
+    cacheName: 'h2h-coach-v300-fapstransfers1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
