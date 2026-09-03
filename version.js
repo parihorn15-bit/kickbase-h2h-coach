3be760ac6f5a1bc85c2b52a8e691ed1b52c71a45
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 22:02:13 +0200

    Bump ownership status asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300ownership1',
-    cacheName: 'h2h-coach-v300-ownership1'
+    assetKey: '300ownership2',
+    cacheName: 'h2h-coach-v300-ownership2'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
