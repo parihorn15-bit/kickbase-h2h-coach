c77397bf5fe7a48fbac249ba722cb7651f901a5e
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 21:28:11 +0200

    Publish complete manager screenshot history

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300fapstransfers1',
-    cacheName: 'h2h-coach-v300-fapstransfers1'
+    assetKey: '300allhistory1',
+    cacheName: 'h2h-coach-v300-allhistory1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
