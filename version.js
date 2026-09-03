df0f0c8f78227ec92d59d240cb4c9548beedbcf9
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 22:38:53 +0200

    Bump transfer screenshot import asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300hornhistory2',
-    cacheName: 'h2h-coach-v300-hornhistory2'
+    assetKey: '300transferimport1',
+    cacheName: 'h2h-coach-v300-transferimport1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
