54e42bb83e329ab1f67d5ff98298863202267053
parihorn15-bit <pari.horn15@gmail.com>
2026-09-03 22:05:19 +0200

    Bump historical player registry asset key

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
     version: '3.0.0',
-    assetKey: '300ownership2',
-    cacheName: 'h2h-coach-v300-ownership2'
+    assetKey: '300historyregistry1',
+    cacheName: 'h2h-coach-v300-historyregistry1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
