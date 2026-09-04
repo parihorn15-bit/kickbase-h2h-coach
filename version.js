9fdc73fcff9015356c636c837e7c83e1c93dbd30
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 09:54:22 +0200

    Bump runtime to 3.0.1 performance build

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.0',
-    assetKey: '300statecore4',
-    cacheName: 'h2h-coach-v300-statecore4'
+    version: '3.0.1',
+    assetKey: '301perf1',
+    cacheName: 'h2h-coach-v301-perf1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
