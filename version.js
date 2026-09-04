7af3e771bfcd58a52662d3c7a6e7ee7572d5bac5
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 11:06:02 +0200

    Release transfer module consolidation 3.0.8

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.7',
-    assetKey: '307runtimecleanup1',
-    cacheName: 'h2h-coach-v307-runtimecleanup1'
+    version: '3.0.8',
+    assetKey: '308transferconsolidation1',
+    cacheName: 'h2h-coach-v308-transferconsolidation1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
