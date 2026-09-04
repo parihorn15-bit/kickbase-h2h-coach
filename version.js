597bf3fb82c2395eb21bfc9f04ecf252318e73f0
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 10:47:38 +0200

    Release transfer single-source cleanup 3.0.6

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.5',
-    assetKey: '305sourcesync1',
-    cacheName: 'h2h-coach-v305-sourcesync1'
+    version: '3.0.6',
+    assetKey: '306singlesource1',
+    cacheName: 'h2h-coach-v306-singlesource1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
