bb74338bd60d8615e9fc35a595e6d7c63e27f268
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 11:14:37 +0200

    Release ownership and validation consolidation 3.0.9

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.8',
-    assetKey: '308transferconsolidation1',
-    cacheName: 'h2h-coach-v308-transferconsolidation1'
+    version: '3.0.9',
+    assetKey: '309ownershipvalidation1',
+    cacheName: 'h2h-coach-v309-ownershipvalidation1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
