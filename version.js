405c6f5cee8a25e2000d9e19b5cd208b8a5126a0
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 10:35:07 +0200

    Release authoritative transfer source cutover 3.0.5

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.4',
-    assetKey: '304datacut1',
-    cacheName: 'h2h-coach-v304-datacut1'
+    version: '3.0.5',
+    assetKey: '305sourcesync1',
+    cacheName: 'h2h-coach-v305-sourcesync1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
