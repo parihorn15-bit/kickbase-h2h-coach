d26df13a518e88ab038d3c8e6c9ad8b4c4cac662
parihorn15-bit <pari.horn15@gmail.com>
2026-09-04 11:00:35 +0200

    Release runtime cleanup 3.0.7

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (() => {
   const release = Object.freeze({
-    version: '3.0.6',
-    assetKey: '306singlesource1',
-    cacheName: 'h2h-coach-v306-singlesource1'
+    version: '3.0.7',
+    assetKey: '307runtimecleanup1',
+    cacheName: 'h2h-coach-v307-runtimecleanup1'
   });
   globalThis.H2H_RELEASE = release;
   globalThis.H2H_APP_VERSION = release.version;
